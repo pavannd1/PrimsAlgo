21133b03f33d83c4dc0e42e53a901ac5c7efd5ae
pavannd1 <pavannd1@gmail.com>
2014-01-26 01:09:05 -0500

    Create adjlist.h

---
+++ adjlist.h
@@ -0,0 +1,42 @@
+#ifndef ADJLIST_H_
+#define ADJLIST_H_
+#include <iostream>
+using namespace std;
+
+/* Class Neighbor defines objects of an edge
+ * it stores source vertex, destination vertex and the cost along with
+ * a pointer to the next edge node
+ */
+class Neighbor
+{
+public:
+	int dest, cost, source;
+	Neighbor* next;
+	Neighbor(int v, int c, int s, Neighbor* n)
+	{
+		dest = v;
+		cost =  c;
+		source = s;
+		next = n;
+	}
+	~Neighbor(){}
+};
+
+/* Class vertex defines each vertex
+ * each vertex has an adjacent list which points to the neighboring vertices
+ */
+class Vertex
+{
+public:
+	int v_num;
+	Neighbor* adjlist;
+	Vertex() {}
+	Vertex(int num, Neighbor* n)
+	{
+		v_num = num;
+		adjlist = n;
+	}
+	~Vertex(){}
+};
+
+#endif /*ADJLIST_H_*/
